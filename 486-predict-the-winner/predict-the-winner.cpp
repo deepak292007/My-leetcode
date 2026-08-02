3d55830e5cc3c5b905742cc99b078809bca8e317
B.R.M.PHANI DEEPAK <deepak.24bcb7227@vitapstudent.ac.in>
2026-08-02 22:32:30 +0530

    Time: 2 ms (43.99%) | Memory: 10.2 MB (44.98%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+int solve(int i ,int j ,vector<int>&nums,vector<vector<int>>&dp){
+    if(i==j)
+    return nums[i];
+    if(dp[i][j] !=  INT_MIN)
+    return dp[i][j];
+    
+    int takeleft = nums[i]-solve(i+1,j,nums,dp);
+    int takeright = nums[j]-solve(i,j-1,nums,dp);
+    return dp[i][j] = max(takeleft,takeright);
+}
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>>dp(n,vector<int>(n,INT_MIN));
+        return solve(0,n-1,nums,dp)>=0;
+        
+    }
+};
